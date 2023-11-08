a55a20393db62dcee0f3676858a63f4de5618f97
Kip <Kipling.Dunlap@gmail.com>
2023-11-07 21:46:30 -0500

    leaflet not working

---
+++ src/lib/Map.svelte
@@ -0,0 +1,28 @@
+<script>
+  import L from 'leaflet';
+  import 'leaflet/dist/leaflet.css';
+
+  let map;
+  let mapElement;
+
+  onMount(() => {
+    map = L.map(mapElement);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
+  });
+
+  onDestroy(() => {
+    map?.remove();
+    map = undefined;
+  });
+</script>
+
+<div bind:this={mapElement}>
+  {#if map}
+    <slot />
+  {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,33 +1,48 @@
 <script>
+  import Map from '../lib/Map.svelte';
   export let data;
   let arrivals = data.RailArrivals;
 </script>
 
 <h1>MARTA Arrivals</h1>
-
-{#each arrivals as arrival}
-  <div class={arrival.LINE}>
-    <p>Destination: {arrival.DESTINATION}</p>
-    <p>Direction: {arrival.DIRECTION}</p>
-    <p>Event Time: {arrival.EVENT_TIME}</p>
-    <p>Head Sign:{arrival.HEAD_SIGN}</p>
-    <p>Line: {arrival.LINE}</p>
-    <p>Next Arrival: {arrival.NEXT_ARR}</p>
-    <p>Station: {arrival.STATION}</p>
-    <p>TrainID: {arrival.TRAIN_ID}</p>
-    <p>Waiting: {arrival.WAITING_SECONDS}</p>
-    <p>WaitingTime: {arrival.WAITING_TIME}</p>
-    <p>Response timestamp: {arrival.RESPONSETIMESTAMP}</p>
-    <p>X: {arrival.VEHICLELONGITUDE}</p>
-    <p>Y: {arrival.VEHICLELATITUDE}</p>
-    <p>Delay: {arrival.DELAY}</p>
-    <p>TripID: {arrival.TRIP_ID}</p>
+<div id="container">
+  <div>
+    {#each arrivals as arrival}
+      <div class="arrs {arrival.LINE}">
+        <p>Destination: {arrival.DESTINATION}</p>
+        <p>Direction: {arrival.DIRECTION}</p>
+        <p>Event Time: {arrival.EVENT_TIME}</p>
+        <p>Head Sign:{arrival.HEAD_SIGN}</p>
+        <p>Line: {arrival.LINE}</p>
+        <p>Next Arrival: {arrival.NEXT_ARR}</p>
+        <p>Station: {arrival.STATION}</p>
+        <p>TrainID: {arrival.TRAIN_ID}</p>
+        <p>Waiting: {arrival.WAITING_SECONDS}</p>
+        <p>WaitingTime: {arrival.WAITING_TIME}</p>
+        <p>Response timestamp: {arrival.RESPONSETIMESTAMP}</p>
+        <p>X: {arrival.VEHICLELONGITUDE}</p>
+        <p>Y: {arrival.VEHICLELATITUDE}</p>
+        <p>Delay: {arrival.DELAY}</p>
+        <p>TripID: {arrival.TRIP_ID}</p>
+      </div>
+      <br />
+    {/each}
   </div>
-  <br />
-{/each}
+  <div>
+    <Map />
+  </div>
+</div>
 
 <style>
-  div {
+  #container {
+    display: flex;
+    justify-content: space-between;
+  }
+  #map {
+    height: 40svw;
+    width: 50svw;
+  }
+  .arrs {
     border: 1px solid black;
     margin: 10px;
     padding: 10px;
