be263affe41a10795644fc567fc6858b23fbe662
Kip <Kipling.Dunlap@gmail.com>
2024-10-30 15:22:13 -0400

    new endpoint, cannot load leaflet map

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,16 +1,19 @@
+// export const ssr = false;
 import { apiKey } from '$env/static/private';
 
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-export async function load({ context, params }) {
+export async function load({}) {
 
   // perform fetech
 
   // Fetch data from API
-  const res = await fetch(`https://developerservices.itsmarta.com:18096/railrealtimearrivals?apiKey=${apiKey}`);
+  const res = await fetch(`https://developerservices.itsmarta.com:18096/itsmarta/
+railrealtimearrivals/traindata?apiKey=${apiKey}`);
 
   const data = await res.json();
 
   // Return data as JSON
-  return data;
+  return {data};
+  // console.log(data);
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,35 @@
 <script>
-  import LeafletMap from 'svelte-leafletjs';
   export let data;
-  let arrivals = data.RailArrivals;
+  let arrivals = data.data;
 </script>
 
 <h1>MARTA Arrivals</h1>
 <div id="container">
   <div>
     {#each arrivals as arrival}
-      <div class="arrs {arrival.LINE}">
-        <p>Destination: {arrival.DESTINATION}</p>
-        <p>Direction: {arrival.DIRECTION}</p>
-        <p>Event Time: {arrival.EVENT_TIME}</p>
-        <p>Head Sign:{arrival.HEAD_SIGN}</p>
-        <p>Line: {arrival.LINE}</p>
-        <p>Next Arrival: {arrival.NEXT_ARR}</p>
-        <p>Station: {arrival.STATION}</p>
-        <p>TrainID: {arrival.TRAIN_ID}</p>
-        <p>Waiting: {arrival.WAITING_SECONDS}</p>
-        <p>WaitingTime: {arrival.WAITING_TIME}</p>
-        <p>Response timestamp: {arrival.RESPONSETIMESTAMP}</p>
-        <p>X: {arrival.VEHICLELONGITUDE}</p>
-        <p>Y: {arrival.VEHICLELATITUDE}</p>
-        <p>Delay: {arrival.DELAY}</p>
-        <p>TripID: {arrival.TRIP_ID}</p>
-      </div>
-      <br />
+      {#if arrival.LINE == 'RED'}
+        <div class="arrs {arrival.LINE}">
+          <p>Destination: {arrival.DESTINATION}</p>
+          <p>Direction: {arrival.DIRECTION}</p>
+          <p>Event Time: {arrival.EVENT_TIME}</p>
+          <!-- <p>Head Sign:{arrival.HEAD_SIGN}</p> -->
+          <p>Line: {arrival.LINE}</p>
+          <p>Next Arrival: {arrival.NEXT_ARR}</p>
+          <p>Station: {arrival.STATION}</p>
+          <p>TrainID: {arrival.TRAIN_ID}</p>
+          <p>Waiting: {arrival.WAITING_SECONDS}</p>
+          <p>WaitingTime: {arrival.WAITING_TIME}</p>
+          <!-- <p>Response timestamp: {arrival.RESPONSETIMESTAMP}</p>
+          <p>X: {arrival.VEHICLELONGITUDE}</p>
+          <p>Y: {arrival.VEHICLELATITUDE}</p>
+          <p>Delay: {arrival.DELAY}</p> -->
+          <p>TripID: {arrival.TRIP_ID}</p>
+        </div>
+        <br />
+        {/if}
     {/each}
   </div>
-  <LeafletMap />
+  <!-- <LeafletMap /> -->
   <div />
 </div>
 
